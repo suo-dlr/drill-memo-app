a3f60ad4c81b3b129e2661a0b9c8b8c2ffe8dba5
suo-dlr <suo.dlr.4r@gmail.com>
2025-05-09 07:22:23 +0900

    add user authentication

--- src/db/access.py
+++ src/db/access.py
@@ -1,10 +1,27 @@
 import datetime
+import os
+from datetime import timedelta
 
+from dotenv import load_dotenv
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.schema import Column
-from sqlalchemy.types import INTEGER, TEXT, TIMESTAMP
+from sqlalchemy.types import INTEGER, TEXT, TIMESTAMP, VARCHAR
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+load_dotenv()
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALG = "HS256"
 
 dialect = "mysql"
 username = "root"
@@ -14,6 +31,9 @@ port = "3306"
 database = "db_memo"
 engine = create_engine(f"{dialect}://{username}:{password}@{host}:{port}/{database}")
 Base = declarative_base()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 class Memo(Base):
@@ -25,6 +45,13 @@ class Memo(Base):
     updated_at = Column(
         TIMESTAMP, default=datetime.datetime.now, onupdate=datetime.datetime.now
     )
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(INTEGER, primary_key=True)
+    username = Column(VARCHAR(20), nullable=False, unique=True)
+    password = Column(VARCHAR(100), nullable=False)
 
 
 SessionClass = sessionmaker(engine)
@@ -63,3 +90,52 @@ def delete_memo_by_id(id: int) -> bool:
     session.delete(memo)
     session.commit()
     return True
+
+
+def _exists_user(user: str):
+    return session.query(User).filter(User.username == user).count() >= 1
+
+
+def register_user(user: str, password: str) -> User | None:
+    if _exists_user(user):
+        return None
+
+    password_hash = pwd_context.hash(password)
+    user = User(username=user, password=password_hash)
+    session.add(user)
+    session.commit()
+    return user
+
+
+def create_access_token(
+    data: dict[str, str], expires_delta: timedelta = timedelta(minutes=15)
+):
+    to_encode = data.copy()
+    expire = datetime.datetime.now() + expires_delta
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALG)
+    print(encoded_jwt)
+    return encoded_jwt
+
+
+def login_user(user: str, password: str) -> str | None:
+    row = session.query(User.password).filter(User.username == user).first()
+    if row is None:
+        return None
+    hashed = row.tuple()[0]
+    if pwd_context.verify(password, hashed):
+        return create_access_token({"sub": user})
+    else:
+        return None
+
+
+def verify_token(token: str) -> bool:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALG])
+        username = payload.get("sub")
+        if username is None:
+            return False
+    except Exception:
+        return False
+    return _exists_user(username)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
-from fastapi import FastAPI, HTTPException
+import re
 
-from db import delete_memo_by_id, insert_memo, select_memo_by_id, update_memo_by_id
+from fastapi import FastAPI, Header, HTTPException
+
+from db import (
+    delete_memo_by_id,
+    insert_memo,
+    login_user,
+    register_user,
+    select_memo_by_id,
+    update_memo_by_id,
+    verify_token,
+)
 from protocols import (
     GetResponse,
     InsertQuery,
     InsertResponse,
+    LoginQuery,
+    LoginResponse,
+    RegisterQuery,
+    RegisterResponse,
     UpdateQuery,
     UpdateResponse,
 )
@@ -13,13 +27,31 @@ app = FastAPI()
 
 
 @app.post("/api/memos", response_model=InsertResponse, status_code=201)
-async def insert(query: InsertQuery):
+async def insert(query: InsertQuery, authorization: str | None = Header(default=None)):
+    if authorization is None:
+        raise HTTPException(401)
+    token = re.match("Bearer\s(\S*)", authorization)
+    if token is None:
+        raise HTTPException(401)
+    print(token.group(1))
+    if not verify_token(token.group(1)):
+        raise HTTPException(401)
+
     memo = insert_memo(query.title, query.content)
     return InsertResponse.model_validate(memo)
 
 
 @app.get("/api/memos/{id}", response_model=GetResponse, status_code=200)
-async def get_memo(id: int):
+async def get_memo(id: int, authorization: str | None = Header(default=None)):
+    if authorization is None:
+        raise HTTPException(401)
+    token = re.match("Bearer\s(\S*)", authorization)
+    if token is None:
+        raise HTTPException(401)
+    print(token.group(1))
+    if not verify_token(token.group(1)):
+        raise HTTPException(401)
+
     memo = select_memo_by_id(id)
     if memo is not None:
         return GetResponse.model_validate(memo)
@@ -28,7 +60,18 @@ async def get_memo(id: int):
 
 
 @app.put("/api/memos/{id}", response_model=UpdateResponse, status_code=200)
-async def update_memo(id: int, query: UpdateQuery):
+async def update_memo(
+    id: int, query: UpdateQuery, authorization: str | None = Header(default=None)
+):
+    if authorization is None:
+        raise HTTPException(401)
+    token = re.match("Bearer\s(\S*)", authorization)
+    if token is None:
+        raise HTTPException(401)
+    print(token.group(1))
+    if not verify_token(token.group(1)):
+        raise HTTPException(401)
+
     memo = update_memo_by_id(id, query.title, query.content)
     if memo is None:
         raise HTTPException(status_code=404)
@@ -37,6 +80,32 @@ async def update_memo(id: int, query: UpdateQuery):
 
 
 @app.delete("/api/memos/{id}", status_code=204)
-async def delete_memo(id: int):
+async def delete_memo(id: int, authorization: str | None = Header(default=None)):
+    if authorization is None:
+        raise HTTPException(401)
+    token = re.match("Bearer\s(\S*)", authorization)
+    if token is None:
+        raise HTTPException(401)
+    if not verify_token(token.group(1)):
+        raise HTTPException(401)
+
     if not delete_memo_by_id(id):
         raise HTTPException(status_code=404)
+
+
+@app.post("/api/users/register", status_code=201)
+def register(query: RegisterQuery):
+    user = register_user(query.username, query.password)
+    if user is None:
+        raise HTTPException(status_code=409)
+    else:
+        return RegisterResponse.model_validate(user)
+
+
+@app.post("/api/users/login", response_model=LoginResponse, status_code=200)
+def login(query: LoginQuery):
+    token = login_user(query.username, query.password)
+    if token is None:
+        raise HTTPException(status_code=401)
+    else:
+        return LoginResponse(token=token)
